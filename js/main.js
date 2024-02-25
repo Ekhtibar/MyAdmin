ed1091240707573ac680dd0cb94bd66db387197a
ehtibar-samedov <e.semedov230@gmail.com>
2024-02-25 11:33:30 +0300

    Базовая структура готова

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Profil BTN
+$(document).ready(function() {
+    $('.profil').click(function() {
+        $('.profil-setting').toggleClass('profilclass');
+    });
+});
+
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
